10acd839b11519c960883e23b8a833654bc91232
Evgenii Grigorev <eagrigorev87@gmail.com>
2022-03-12 12:05:06 +0300

    Dummy backend created

--- server/server.js
+++ server/server.js
@@ -1,8 +1,59 @@
-const PORT = require('../utils/const');
+const PORT = require('./utils/const');
+const { userJoin, userLeft, userFind } = require('./utils/user');
+const { createMessage } = require('./utils/message');
+
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
+const socket = require('socket.io');
+const moment = require('moment');
 
 const app = express();
-app.listen(PORT, () => {
+app.use(express());
+app.use(cors());
+
+var server = app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}...`);
+});
+
+const io = socket(server);
+
+// Socket connection initialisation
+io.on('connection', (socket) => {
+  // New user joining the room
+  socket.on("userJoin", ({ name, room }) => {
+    const user = userJoin(socket.id, name, room);
+    socket.join(user.room);
+
+    // Welcome message to a newcomer
+    socket.emit(
+      'message',
+      createMessage('Bot', `Welcome, ${user.name}!`)
+    );
+
+    // Notify message to everyone in that room
+    socket.broadcast.to(user.room).emit(
+      'message',
+      createMessage('Bot', `${user.name} has joined the chat.`)
+    );
+  });
+
+  // Someone is sending a message
+  socket.on('messaging', (message) => {
+    const user = userFind(socket.id);
+    io.to(user.room).emit(
+      'message',
+      createMessage(user.name, message)
+    );
+  });
+
+  // Someone is leaving the room or closing the app
+  socket.on('disconnect', () => {
+    const user = userLeft(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        createMessage('Bot', `${user.name} has left the chat.`)
+      );
+    };
+  });
 });
